13243823b51227d0bcb96eb54981aef8b6c8cb78
João Gabriel Macedo de Paula <jgmapaula@gmail.com>
2026-02-05 11:08:19 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-self.addEventListener('fetch', function(event) {});
+const assets = [
+  "/",
+  "/index.html",
+  "/pwa.json",
+  "/icone.png",
+  "/admin/index.html",    // Caminho atualizado
+  "/admin/pwa_admin.json", // Caminho atualizado
+  "/admin/icone_admin.png"  // Caminho atualizado
+];
